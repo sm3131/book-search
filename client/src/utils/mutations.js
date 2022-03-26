5e5348617cffc8db5d6f5b31519632bae726d3c0
sm3131 <sm2683@nau.edu>
2022-03-25 21:32:46 -0700

    created apollo client, and set-up queries and mutations in utils folder

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,84 @@
+import { gql } from '@apollo/client';
+
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+        bookCount
+        savedBooks {
+            bookId
+            title
+            authors
+            description
+            link
+            image
+        }
+      }
+    }
+  }
+`;
+
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+        bookCount
+        savedBooks {
+            bookId
+            title
+            authors
+            description
+            link
+            image
+        }
+      }
+    }
+  }
+`;
+
+export const SAVE_BOOK = gql`
+  mutation saveBook($input: SaveBookInput) {
+    saveBook(input: $input) {
+      user {
+        _id
+        username
+        bookCount
+        savedBooks {
+            bookId
+            title
+            authors
+            description
+            link
+            image
+        }
+      }
+    }
+  }
+`;
+
+
+export const REMOVE_BOOK = gql`
+  mutation removeBook($bookId: String!) {
+    removeBook(bookId: $bookId) {
+      user {
+        _id
+        username
+        bookCount
+        savedBooks {
+            bookId
+            title
+            authors
+            description
+            link
+            image
+        }
+      }
+    }
+  }
+`;
